862198b35e91660e8379c3c96a8c6d6e40fddffa
Gabri177 <forestcat177@gmail.com>
2025-01-08 11:48:56 +0100

    add addUser func

--- src/components/DiaLog.vue
+++ src/components/DiaLog.vue
@@ -1,10 +1,10 @@
 <template>
 	<Teleport to="body">
-		<el-dialog v-model="centerDialogVisible" :title="title" :width="width" align-center>
+		<el-dialog v-model="centerDialogVisible" :title="title" :width="width" style="max-height: 500px; overflow-y: auto; " align-center>
 			<slot></slot>
 			<template #footer>
 				<div class="flex justify-between items-center">
-					<div><el-button type="warning" @click="handleDelete">删除用户</el-button></div>
+					<div><el-button type="warning" @click="handleDelete" v-show="isVisibleDelete">删除用户</el-button></div>
 					<div class="dialog-footer">
 						<el-button @click="handleCancel">取消</el-button>
 						<el-button type="primary" @click="handleConfirm" :loading="loading">
@@ -59,6 +59,10 @@ defineProps({
 	width: {
 		type: Number,
 		default: 500
+	},
+	isVisibleDelete: {
+		type: Boolean,
+		default: true
 	}
 })
 
@@ -74,3 +78,20 @@ defineExpose({
 
 
 </script>
+
+
+<style>
+/* 自定义滚动条样式 */
+::-webkit-scrollbar {
+  width: 4px; /* 滚动条宽度 */
+  background-color: #f1f1f1; /* 滚动条背景色 */
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #888; /* 滑块颜色 */
+  border-radius: 4px; /* 滑块圆角 */
+}
+::-webkit-scrollbar-thumb:hover {
+  background-color: #555; /* 鼠标悬停时滑块颜色 */
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,9 +9,8 @@
 					<span>Super Admin</span>
 				</div>
 				<div class="flex items-center justify-center">
-					<el-input v-model="searchInput" style="width: 240px" placeholder="Find your item" :prefix-icon="Search"
-					class="mr-3 "
-					/>
+					<el-input v-model="searchInput" style="width: 240px" placeholder="Find your item"
+						:prefix-icon="Search" class="mr-3 " />
 					<el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
 						class="mr-4 cursor-pointer" style="height: 30px; width: 30px;" @click="handleAvatarClick" />
 					<el-dropdown @command="handleCommand" class="mt-1">
@@ -31,70 +30,50 @@
 			</el-header>
 			<el-main class="fixed top-[64px] w-full max-h-[calc(100% - 64px)]]">
 				<div>
-					<el-table :data="filteredTableData" style="width: 100%; height: calc(100vh - 144px)"
-					>
+					<el-table :data="filteredTableData" style="width: 100%; height: calc(100vh - 144px)">
 						<el-table-column fixed prop="date" label="Activation date" width="150" sortable />
 						<el-table-column label="User Name" width="180">
 							<template #default="scope">
-								<el-popover effect="light" trigger="hover" placement="top" >
+								<el-popover effect="light" trigger="hover" placement="top">
 									<template #default>
 										<div>Name: {{ scope.row.name }}</div>
-										<div>Address: {{ scope.row.address }}</div>
+										<div>Id: {{ scope.row.id }}</div>
 									</template>
 									<template #reference>
-										<el-tag >{{ scope.row.name }}</el-tag>
+										<el-tag>{{ scope.row.name }}</el-tag>
 									</template>
 								</el-popover>
 							</template>
 						</el-table-column>
-						<el-table-column prop="state" label="State" sortable  >
+						<el-table-column prop="state" label="State" sortable>
 							<template #default="scope">
-								<TagState :initialState="scope.row.state" @change="handleStateChange(scope.row, $event)" />
-							</template>	
-						</el-table-column>
-						<el-table-column label="Remaining Days">
-							<template #default="scope">
-								<!-- <div  class="flex items-center justify-center"> -->
-									{{ calculateDays(scope.row.date, scope.row.expireDate) > 0 ? calculateDays(scope.row.date, scope.row.expireDate) : 0 }}
-								<!-- </div> -->
+								<TagState :initialState="scope.row.state"
+									@change="handleStateChange(scope.row, $event)" />
 							</template>
 						</el-table-column>
+						<el-table-column prop="remainingDays" label="Remaining Days" sortable>
+						</el-table-column>
 						<el-table-column prop="expireDate" label="Expire Date" sortable />
-						<el-table-column fixed="right" label="Operations"  align="right">
+						<el-table-column fixed="right" label="Operations" align="right">
 							<template #default="scope">
-								
-									<el-button size="small" @click="handleEdit(scope.row)" style="width: 70px">
-										编辑
-									</el-button>
-									<DiaLog 
-									ref="editDialog"
-									title="编辑"
-									@confirm="handleEditConfirm"
-									@cancel="handleEditCancel"
-									@delete="handleEditDelete"
-									>
-										<span class="font-bold block mb-3">选择时间区间: </span>
-										<div class="flex justify-center items-center mb-3">
-											<el-date-picker
-											v-model="dateRange"
-											type="datetimerange"
-											range-separator="到"
-											start-placeholder="Start date"
-											end-placeholder="End date"
-											:default-value="[new Date(), new Date()]"
-											:popper-append-to-body="true"
-											/>
-										</div>
-										<span class="block font-bold mb-2">编辑备注:</span>
-										<div>
-											<el-input
-											type="textarea"
-											rows="3"
-											placeholder="Please input your remarks"
-											v-model="selectedRow.note"
-											/>
-										</div>
-									</DiaLog>
+
+								<el-button size="small" @click="handleEdit(scope.row)" style="width: 70px">
+									编辑
+								</el-button>
+								<DiaLog ref="editDialog" title="编辑" @confirm="handleEditConfirm"
+									@cancel="handleEditCancel" @delete="handleEditDelete">
+									<span class="font-bold block mb-3">选择时间区间: </span>
+									<div class="flex justify-center items-center mb-3">
+										<el-date-picker v-model="dateRange" type="datetimerange" range-separator="到"
+											start-placeholder="Start date" end-placeholder="End date"
+											:default-value="[new Date(), new Date()]" :popper-append-to-body="true" />
+									</div>
+									<span class="block font-bold mb-2">编辑备注:</span>
+									<div>
+										<el-input type="textarea" rows="3" placeholder="Please input your remarks"
+											v-model="selectedRow.note" />
+									</div>
+								</DiaLog>
 							</template>
 
 						</el-table-column>
@@ -102,14 +81,47 @@
 					</el-table>
 				</div>
 				<div class=" min-h-[64px] relative overflow-visible" style="z-index: auto;">
-					<el-button class="mt-4" style="width: 100%" @click="onAddItem">
+					<el-button class="mt-4" style="width: 100%" @click="onAddItem" color="#2d4765" :dark="false">
 						添加新用户
 					</el-button>
 				</div>
 			</el-main>
 		</el-container>
 	</div>
-	
+	<DiaLog ref="addDialog" title="添加用户" @confirm="handleAddConfirm" @cancel="handleAddCancel" :isVisibleDelete="false">
+
+		<div>
+			<span class="subTitle">用户名:</span>
+			<div>
+				<el-input v-model="newUser.name" />
+			</div>	
+		</div>
+		<div>
+			<span class="subTitle">选择时间区间: </span>
+			<div class="flex justify-center items-center mb-3">
+				<el-date-picker v-model="dateRange" type="datetimerange" range-separator="到" start-placeholder="Start date"
+					end-placeholder="End date" :default-value="[new Date(), new Date()]" :popper-append-to-body="true" />
+			</div>
+		</div>
+		<div>
+			<span class="subTitle">选择激活状态:</span>
+			<div class="flex mx-2">
+				<div class="flex items-center">
+					<el-radio v-model="newUser.state" label="未激活">未激活</el-radio>
+					<el-radio v-model="newUser.state" label="已激活">已激活</el-radio>
+					<el-radio v-model="newUser.state" label="已过期">已过期</el-radio>
+				</div>
+			</div>
+		</div>
+		<div>
+			<span class="subTitle">编辑备注:</span>
+			<div>
+				<el-input type="textarea" rows="3" placeholder="在这里输入对用户的备注信息" v-model="newUser.note" />
+			</div>
+		</div>
+		
+		
+	</DiaLog>
 </template>
 
 <script setup>
@@ -127,45 +139,100 @@ import { useRouter } from 'vue-router'
 const now = new Date()
 const searchInput = ref('')
 const editDialog = ref(null)
+const addDialog = ref(null)
 const selectedRow = ref(null);
 const router = useRouter()
+
+const newUser = reactive({
+	date: '',
+	name: '',
+	state: '未激活',
+	expireDate: '',
+	note: ''
+})
+
+
+
 const tableData = reactive([
 	{
+		id: 1,
 		date: '2016-05-03',
 		name: 'Tom1',
 		state: '已激活',
 		expireDate: '2016-05-01',
-		tag: 'Home',
-		note: 'This is a note'
+		note: 'This is a note',
+		remainingDays: 0
 	},
 	{
+		id: 2,
 		date: '2016-05-02',
 		name: 'Tom2',
 		state: '已过期',
 		expireDate: '2017-05-02',
-		tag: 'Office',
-		note: 'This is a note'
+		note: 'This is a note',
+		remainingDays: 0
 	},
 	{
+		id: 3,
 		date: '2016-05-04',
 		name: 'Tom3',
 		state: '未激活',
 		expireDate: '2017-05-03',
-		tag: 'Home',
-		note: 'This is a note'
+		note: 'This is a note',
+		remainingDays: 0
 	}
-])
+]);
+
+function initializeRemainingDays() {
+
+	//应该有一个getData方法 从后端获取数据
+	tableData.forEach((row) => {
+		row.remainingDays = calculateDays(row.date, row.expireDate) > 0
+			? calculateDays(row.date, row.expireDate)
+			: 0;
+	});
+}
+
+initializeRemainingDays();
+
+
 const dateRange = ref([]);
 
 
 const filteredTableData = computed(() => {
-  if (!searchInput.value) return tableData; // 如果没有搜索关键字，返回全部数据
-  return tableData.filter((item) => 
-    Object.values(item).some(value =>
-      value.toString().toLowerCase().includes(searchInput.value.toLowerCase())
-    )
-  );
+	// 如果没有搜索关键字，返回全部数据
+	if (!searchInput.value) return tableData; 
+	return tableData.filter((item) =>
+		Object.values(item).some(value =>
+			value.toString().toLowerCase().includes(searchInput.value.toLowerCase())
+		)
+	);
 });
+
+const handleAddConfirm = () => {
+
+	if (!formValidate()) return
+	newUser.date = dayjs(dateRange.value[0]).format('YYYY-MM-DD')
+	newUser.expireDate = dayjs(dateRange.value[1]).format('YYYY-MM-DD')
+	tableData.push({
+		date: newUser.date,
+		name: newUser.name,
+		state: newUser.state,
+		expireDate: newUser.expireDate,
+		note: newUser.note,
+		remainingDays: calculateDays(newUser.date, newUser.expireDate)
+	})
+	addDialog.value.closeDialog()
+
+	initializeRemainingDays();
+}
+
+const handleAddCancel = () => {
+	console.log('handleAddCancel')
+	addDialog.value.closeDialog()
+}
+
+
 
 
 const handleEditConfirm = () => {
@@ -173,6 +240,8 @@ const handleEditConfirm = () => {
 	selectedRow.value.date = dayjs(dateRange.value[0]).format('YYYY-MM-DD')
 	selectedRow.value.expireDate = dayjs(dateRange.value[1]).format('YYYY-MM-DD')
 	editDialog.value.closeDialog()
+
+	initializeRemainingDays();
 }
 
 const handleEditCancel = () => {
@@ -182,20 +251,22 @@ const handleEditCancel = () => {
 
 const handleEditDelete = () => {
 	popOut('警告', '确定删除该用户吗？', "确认", "取消")
-	.then(() => {
-		setTimeout(() => {
-			tableData.find((item, index) => {
-				if (item === selectedRow.value) {
-					tableData.splice(index, 1)
-					editDialog.value.closeDialog()
-				}
-			})
-		}, 500);
-		toast('成功', '删除成功')
-	})
-	.catch((error) => {
-		console.log('cancel error', error)
-	})
+		.then(() => {
+			setTimeout(() => {
+				tableData.find((item, index) => {
+					if (item === selectedRow.value) {
+						tableData.splice(index, 1)
+						editDialog.value.closeDialog()
+					}
+				})
+			}, 500);
+			toast('成功', '删除成功')
+
+			initializeRemainingDays();
+		})
+		.catch((error) => {
+			console.log('cancel error', error)
+		})
 }
 
 const handleStateChange = (row, state) => {
@@ -204,9 +275,9 @@ const handleStateChange = (row, state) => {
 }
 
 const handleEdit = (row) => {
-	selectedRow.value = row; 
+	selectedRow.value = row;
 	dateRange.value = [new Date(row.date), new Date(row.expireDate)];
-	editDialog.value.openDialog()	
+	editDialog.value.openDialog()
 }
 
 const handleAvatarClick = () => {
@@ -221,21 +292,21 @@ const handleCommand = (command) => {
 			break
 		case 'logout':
 			popOut('警告', '确定退出登录吗？', "确认", "取消")
-			.then(() => {
-				logout()
 				.then(() => {
-					toast('成功', '退出登录成功')
-					router.push({ path: '/'})
+					logout()
+						.then(() => {
+							toast('成功', '退出登录成功')
+							router.push({ path: '/' })
 
+						})
+						.catch((error) => {
+							console.log('error', error)
+							toast('错误', '退出登录失败', 'error')
+						})
 				})
 				.catch((error) => {
-					console.log('error', error)
-					toast('错误', '退出登录失败', 'error')
+					console.log('cancel error', error)
 				})
-			})
-			.catch((error) => {
-				console.log('cancel error', error)
-			})
 			break
 		default:
 			break
@@ -247,35 +318,67 @@ const handleClick = () => {
 }
 
 const onAddItem = () => {
-	now.setDate(now.getDate() + 1)
-	tableData.push({
-		date: dayjs(now).format('YYYY-MM-DD'),
-		name: now.getTime(),
-		state: '已激活',
-		expireDate: dayjs(now).format('YYYY-MM-DD'),
-		tag: 'Home',
-		note: 'This is a note'
-	})
+	// now.setDate(now.getDate() + 1)
+	// tableData.push({
+	// 	date: dayjs(now).format('YYYY-MM-DD'),
+	// 	name: now.getTime(),
+	// 	state: '已激活',
+	// 	expireDate: dayjs(now).format('YYYY-MM-DD'),
+	// 	tag: 'Home',
+	// 	note: 'This is a note'
+	// })
+	newUser.name = ''
+	newUser.state = '未激活'
+	newUser.note = ''
+	dateRange.value = [new Date(), new Date()]
+	addDialog.value.openDialog()
 	console.log('tableData', tableData)
+
+	
 }
 function calculateDays(date1, date2) {
-  // 将日期字符串转换为 Date 对象
-  const startDate = new Date(date1);
-  const endDate = new Date(date2);
+	// 将日期字符串转换为 Date 对象
+	const startDate = new Date(date1);
+	const endDate = new Date(date2);
 
-  // 获取两个日期的时间戳
-  const startTime = startDate.getTime();
-  const endTime = endDate.getTime();
+	// 获取两个日期的时间戳
+	const startTime = startDate.getTime();
+	const endTime = endDate.getTime();
 
-  // 计算时间差（毫秒），然后转换为天数
-  const diffTime = endTime - startTime; // 使用绝对值防止负数
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+	// 计算时间差（毫秒），然后转换为天数
+	const diffTime = endTime - startTime; // 使用绝对值防止负数
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-  return diffDays;
+	return diffDays;
+}
+
+function formValidate() {
+	if (!newUser.name) {
+		toast('错误', '用户名不能为空', 'error')
+		return false
+	}
+	if (!dateRange.value[0] || !dateRange.value[1]) {
+		toast('错误', '时间区间不能为空', 'error')
+		return false
+	}
+	const isDuplicate = tableData.some((item) => {
+		if (item.name === newUser.name) {
+			toast('错误', '用户名已存在', 'error');
+			return true; // 停止遍历
+		}
+	});
+	if (isDuplicate) {
+		return false;
+	}
+	return true
 }
 
 </script>
 
 
 <style scoped>
+
+.subTitle {
+	@apply font-bold block mb-2;
+}
 </style>
